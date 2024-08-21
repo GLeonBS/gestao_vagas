30d8b1d7954ab067019db68ba21c70950a6598a6
Gabriel Leon Brugnolo de Souza <gabriel.souza@elotech.com.br>
2024-08-21 13:48:55 -0300

    feat: 🎸 Add swagger para documentação

--- src/main/java/br/com/leon/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/leon/gestao_vagas/GestaoVagasApplication.java
@@ -3,7 +3,15 @@ package br.com.leon.gestao_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Gestão de Vagas",
+		description = "API para gestão de vagas",
+		version = "1.0"
+))
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
